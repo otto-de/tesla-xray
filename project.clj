654f0ef4af0985b8111231c4dceb018049be5c5a
Kai Brandes <kai.brandes@otto.de>
2015-11-12 17:57:13 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-xray "0.2.2"
+(defproject de.otto/tesla-xray "0.2.3"
   :description "a component to execute and visualize checks written in clj"
   :url "https://github.com/otto-de/tesla-x-ray.git"
   :license {:name "Apache License 2.0"
