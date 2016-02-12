0f677a351a16e136876a57aaf8522a35b4ba97cc
Tesla Working Station I (no comment) <tesla@dv.otto.de>
2016-02-12 09:38:46 +0100

    IS/CD| Cleaned up deps.

--- project.clj
+++ project.clj
@@ -9,13 +9,10 @@
                  [ring/ring-codec "1.0.0"]
                  [clj-time "0.11.0"]]
 
-  :provided {:dependencies [[de.otto/tesla-basic-logging "0.1.4"]
-                            [de.otto/tesla-microservice "0.1.18"]]}
-
   :profiles {:dev {:plugins      [[lein-ancient "0.5.4"]]
                    :main de.otto.tesla.xray.testsystem
                    :source-paths ["src" "test"]
-                   :dependencies [[de.otto/tesla-microservice "0.1.18"]
+                   :dependencies [[de.otto/tesla-microservice "0.1.27"]
                                   [me.lomin/component-restart "0.1.0"]
-                                  [de.otto/tesla-jetty "0.1.0"]
+                                  [de.otto/tesla-jetty "0.1.1"]
                                   [ring-mock "0.1.5"]]}})
