96226eeade863b168ebf80c39ecee5ff54bfca5b
carl.duevel <carl.duevel@holisticon.de>
2016-10-13 18:58:20 +0200

    Added release config

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-xray "0.3.26"
+(defproject de.otto/tesla-xray "0.3.26-SNAPSHOT"
   :description "a component to execute and visualize checks written in clj"
   :url "https://github.com/otto-de/tesla-xray.git"
   :license {:name "Apache License 2.0"
@@ -9,7 +9,8 @@
                  [ring/ring-codec "1.0.1"]
                  [clj-time "0.12.0"]]
   :test-paths ["test" "test-resources"]
-  :profiles {:dev {:plugins      [[lein-ancient "0.6.10"]]
+  :lein-release {:deploy-via :clojars}
+  :profiles {:dev {:plugins      [[lein-ancient "0.6.10"][lein-release/lein-release "1.0.9"]]
                    :main de.otto.tesla.xray.testsystem
                    :source-paths ["src" "test"]
                    :dependencies [[de.otto/tesla-microservice "0.3.36"]
