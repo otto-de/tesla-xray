f18afaf99baa4cd6cc62de7ea2758990f9009e3c
Kai Brandes <kai.brandes@otto.de>
2016-10-25 13:34:21 +0200

    a better test-system which has registered checks instead of fixed data

--- test/de/otto/tesla/xray/testsystem.clj
+++ test/de/otto/tesla/xray/testsystem.clj
@@ -8,47 +8,39 @@
             [clojure.tools.logging :as log])
   (:gen-class))
 
-(defn random-tt []
-  (rand-int 100))
+(defn random-result []
+  (case (rand-int 3)
+    0 (chk/->XRayCheckResult :ok "ok")
+    1 (chk/->XRayCheckResult :error "error")
+    2 (chk/->XRayCheckResult :warning "warning")))
 
-(defn tstmp []
-  (let [ten-minutes (* 1000 60 10)]
-    (- (System/currentTimeMillis) (rand-int ten-minutes))))
+(defrecord Check [check-name]
+  c/Lifecycle
+  (start [self]
+    (chkr/register-check (:rt-checker self) self check-name)
+    self)
+  (stop [self] self)
 
-(defn random-result [_]
-  (case (rand-int 4)
-    0 (chk/->XRayCheckResult :ok "ok" (random-tt) (tstmp))
-    1 (chk/->XRayCheckResult :error "error" (random-tt) (tstmp))
-    2 (chk/->XRayCheckResult :warning "warning" (random-tt) (tstmp))
-    3 (chk/->XRayCheckResult :none "no status" (random-tt) (tstmp))))
-
-(defn n-random-results [n]
-  (let [results (into [] (map random-result (range n)))]
-    {:overall-status (chkr/default-strategy results)
-     :results        results}))
-
-(def some-data
-  {"CheckA"                                    {"dev"  (n-random-results 50)
-                                                "test" (n-random-results 100)
-                                                "prod" (n-random-results 10)}
-   "CheckB"                                    {"dev"  (n-random-results 20)
-                                                "test" (n-random-results 60)
-                                                "prod" (n-random-results 20)}
-   "CheckC"                                    {"dev"  (n-random-results 70)
-                                                "test" (n-random-results 40)
-                                                "prod" (n-random-results 30)}
-   "CheckD with special chars / (/)(\\\")%& {" {"dev"  (n-random-results 50)
-                                                "test" (n-random-results 24)
-                                                "prod" (n-random-results 5)}})
+  chk/XRayCheck
+  (start-check [_ _]
+    (random-result)))
 
 (defn test-system [runtime-config]
   (-> (tesla/base-system (assoc runtime-config :name "test-system"))
-      (assoc :rt-checker (c/using (chkr/new-xraychecker "test") [:handler :config]))
+      (assoc
+        :rt-checker (c/using (chkr/new-xraychecker "test") [:handler :config])
+        :check0 (c/using (->Check "check0") [:rt-checker])
+        :check1 (c/using (->Check "check1") [:rt-checker])
+        :check2 (c/using (->Check "check2") [:rt-checker])
+        :check3 (c/using (->Check "check3") [:rt-checker])
+        :check4 (c/using (->Check "check4") [:rt-checker])
+        :check5 (c/using (->Check "check5") [:rt-checker])
+        :check6 (c/using (->Check "check6") [:rt-checker]))
       (serving-with-jetty/add-server :rt-checker)))
 
 (defn -main [& args]
-  (let [{:keys [rt-checker] :as started} (tesla/start (test-system {:test-nr-checks-displayed "1"
-                                                                    :test-check-environments  "dev;test;prod"}))]
-    (reset! (:check-results rt-checker) some-data)
-    (log/info "test-system started")
+  (let [started (tesla/start (test-system {:test-check-frequency     "2000"
+                                           :test-nr-checks-displayed "3"
+                                           :test-check-environments  "dev;test;prod"}))]
+    (log/info "test-system started. Goto http://localhost:8080/xray-checker")
     (restart/watch (var -main) started)))
