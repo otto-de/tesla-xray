5a7642a91027d3fbb6704cda3ba289ae25ec6938
tesla <tesla@dv.otto.de>
2017-07-13 13:18:16 +0200

    dependencies updated

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License 2.0"
             :url  "http://www.apache.org/license/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [de.otto/tesla-microservice "0.7.1"]
+                 [de.otto/tesla-microservice "0.11.2"]
                  [hiccup "1.0.5"]
                  [ring/ring-codec "1.0.1"]
                  [org.clojure/data.xml "0.0.8"]
@@ -18,8 +18,8 @@
                                   [lein-release/lein-release "1.0.9"]]
                    :main         de.otto.tesla.xray.testsystem
                    :source-paths ["src" "test"]
-                   :dependencies [[me.lomin/component-restart "0.1.1"]
+                   :dependencies [[me.lomin/component-restart "0.1.2"]
                                   [de.otto/tesla-jetty "0.1.3"]
                                   [ring-mock "0.1.5"]
-                                  [ch.qos.logback/logback-classic "1.1.9"]
-                                  [clj-cctray "0.10.0"]]}})
+                                  [ch.qos.logback/logback-classic "1.2.3"]
+                                  [clj-cctray "0.12.0"]]}})
