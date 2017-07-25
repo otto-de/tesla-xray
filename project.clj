7d5d184473b52e236ae8d88f804d7a5c8dbfea3f
Carl Düvel <carl.duevel@otto.de>
2017-07-25 13:58:03 +0200

    lein-release plugin: preparing 0.8.5 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-xray "0.8.5-SNAPSHOT"
+(defproject de.otto/tesla-xray "0.8.5"
   :description "a component to execute and visualize checks written in clj"
   :url "https://github.com/otto-de/tesla-xray.git"
   :license {:name "Apache License 2.0"
