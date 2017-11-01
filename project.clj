40e81ca006a8b7c1bdd9082451a913eea863ed26
Carl Düvel <carl.duevel@otto.de>
2017-11-01 15:57:21 +0100

    Upgraded dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License 2.0"
             :url  "http://www.apache.org/license/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [de.otto/tesla-microservice "0.11.7"]
+                 [de.otto/tesla-microservice "0.11.13"]
                  [hiccup "1.0.5"]
                  [ring/ring-codec "1.0.1"]
                  [org.clojure/data.xml "0.0.8"]
@@ -19,7 +19,7 @@
                    :main         de.otto.tesla.xray.testsystem
                    :source-paths ["src" "test"]
                    :dependencies [[me.lomin/component-restart "0.1.2"]
-                                  [de.otto/tesla-jetty "0.1.4"]
+                                  [de.otto/tesla-jetty "0.2.4"]
                                   [ring-mock "0.1.5"]
                                   [ch.qos.logback/logback-classic "1.2.3"]
                                   [clj-cctray "0.12.0"]]}})
