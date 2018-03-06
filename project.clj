f117faac7fe9db4b15f8f136e3f82f992760b266
Christian Stamm <christian.stamm@otto.de>
2018-03-06 10:54:56 +0100

    v0.8.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-xray "0.8.9-SNAPSHOT"
+(defproject de.otto/tesla-xray "0.8.9"
   :description "a component to execute and visualize checks written in clj"
   :url "https://github.com/otto-de/tesla-xray.git"
   :license {:name "Apache License 2.0"
